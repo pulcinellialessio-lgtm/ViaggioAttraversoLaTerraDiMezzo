f1e017c1b0dbd8334c59da5e46c97c1470adf0b0
pulcinellialessio-lgtm <pulcinelli.alessio@franchettisalviani.net>
2025-12-10 22:05:29 +0100

    kjhg

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.Design;
 using System.Data;
 
 namespace ViaggioControBowser
@@ -13,7 +14,6 @@ namespace ViaggioControBowser
 
             Console.WriteLine("\r\n\r\n");
 
-            Console.WriteLine("Sei nei panni di mario, devi riuscire ad arrivare alla fortezza di Bowser (Bowser's kingdom) e salvare la principessa Peach. Qusto viaggio non sarà facile, ci saranno imprevisti nel mezzo del viaggio che potranno cambiare le sorti dell'avventura, ad accompagnarti c'è cappy, un'alleato di mario che permette a lui di possedere personaggi, nemici e oggetti. Quindi detto ciò buona fortuna...");
         }
         static string Personaggi(int vita, int attacco) //Scelta personaggio
         {
@@ -27,6 +27,8 @@ namespace ViaggioControBowser
             Console.WriteLine(" ------------------------------------------");
 
             string personaggi = Console.ReadLine();
+
+            Console.WriteLine("Sei nei panni di " + personaggi + ", devi riuscire ad arrivare alla fortezza di Bowser (Bowser's kingdom) e salvare la principessa Peach. Qusto viaggio non sarà facile, ci saranno imprevisti nel mezzo del viaggio che potranno cambiare le sorti dell'avventura, ad accompagnarti c'è cappy, un'alleato di mario che permette a lui di possedere personaggi, nemici e oggetti. Quindi detto ciò buona fortuna...");
 
             return personaggi;
         }
@@ -60,13 +62,13 @@ namespace ViaggioControBowser
             Random rand = new Random();
             Random Fuga = new Random();
 
-            int DinosauroVita = 10, DinosauroAttacco = 2;
+            int DinosauroVita = 10, DinosauroAttacco = 2, ChainChompVita = 5, ChainChompAttacco = 1, MadameBroodeVita = 20, MadameBroodeAttacco = 2, p = 0, l = 0;
 
             for (int i = 0; i < Fossilandia.Length; i++)
             {
                 if (i == 0) //Dinosauro
                 {
-                    Console.WriteLine("Sei entrato in Fossilandia, all'improvviso un Dinosauro ti blocca la strada! All'improvviso noti una via di fuga!");
+                    Console.WriteLine("Sei entrato in Fossilandia, all'improvviso un Dinosauro ti blocca la strada ma noti una via di fuga!");
 
                     Console.WriteLine(" Cosa scegli di fare? ");
                     Console.WriteLine(" 1: Affrontare il Dinosauro, con possibile ricompensa ");
@@ -84,23 +86,25 @@ namespace ViaggioControBowser
                             if (TiroDado <= 3)
                             {
                                 Console.WriteLine("Valore tiro dado: " + TiroDado);
-                                Console.WriteLine("Non sei riuscito ad attaccare il dinosauro, ora lui ti ha attaccato e ti ha inflitto " + DinosauroAttacco + " danni!");
+                                Console.WriteLine("Non sei riuscito ad attaccare il dinosauro, ora lui ti ha attaccato e ti ha inflitto " + DinosauroAttacco + " danni! Premi un tasto per continuare...");
+                                Console.ReadLine();
                                 vita -= DinosauroAttacco;
                             }
                             else
                             {
                                 Console.WriteLine("Valore tiro dado: " + TiroDado);
-                                Console.WriteLine("Sei riuscito ad attaccare il dinosauro, gli hai inflitto " + attacco + " danni!");
+                                Console.WriteLine("Sei riuscito ad attaccare il dinosauro, gli hai inflitto " + attacco + " danni! Premi un tato per continuare...");
+                                Console.ReadLine();
                                 DinosauroVita -= attacco;
                             }
                         }
 
                         Console.WriteLine("Sei riuscito a stordire il dinosauro! Come ricompensa per la tua vittoria, ottieni una Pozione Cura!");
 
-                        ////////////////////////////
+                        ////////////////////////////inventario
 
                         Console.WriteLine("Ora che il dinosauro è stordito puoi usare cappy per possederlo e riuscire a distruggere ed oltrepassare il muro di roccia che ti ostacola");
-
+                        p++;
                     }
                     else if (scelta == "2")
                     {
@@ -113,26 +117,30 @@ namespace ViaggioControBowser
                         {
                             Console.WriteLine("Valore tiro dado fuga: " + TiroDadoFuga);
                             Console.WriteLine("Sei riuscito a fuggire via dal dinosauro utilizzando la via di fuga!");
+                            l++;
                         }
                         else
                         {
                             Console.WriteLine("Valore tiro dado fuga: " + TiroDadoFuga);
                             Console.WriteLine("Non sei riuscito a fuggire via dal dinosauro, prova ad attaccarlo!");
+                            p++;
 
                             while (DinosauroVita > 0)
                             {
-                                int TiroDado = rand.Next(1, 7);
+                                int TiroDado2 = rand.Next(1, 7);
 
-                                if (TiroDado <= 3)
+                                if (TiroDado2 <= 3)
                                 {
-                                    Console.WriteLine("Valore tiro dado: " + TiroDado);
-                                    Console.WriteLine("Non sei riuscito ad attaccare il dinosauro, ora lui ti ha attaccato e ti ha inflitto " + DinosauroAttacco + " danni!");
+                                    Console.WriteLine("Valore tiro dado: " + TiroDado2);
+                                    Console.WriteLine("Non sei riuscito ad attaccare il dinosauro, ora lui ti ha attaccato e ti ha inflitto " + DinosauroAttacco + " danni! Premi un tasto per continuare...");
+                                    Console.ReadLine();
                                     vita -= DinosauroAttacco;
                                 }
                                 else
                                 {
-                                    Console.WriteLine("Valore tiro dado: " + TiroDado);
-                                    Console.WriteLine("Sei riuscito ad attaccare il dinosauro, gli hai inflitto " + attacco + " danni!");
+                                    Console.WriteLine("Valore tiro dado: " + TiroDado2);
+                                    Console.WriteLine("Sei riuscito ad attaccare il dinosauro, gli hai inflitto " + attacco + " danni! Premi un tasto per continuare...");
+                                    Console.ReadLine();
                                     DinosauroVita -= attacco;
                                 }
                             }
@@ -148,14 +156,108 @@ namespace ViaggioControBowser
                 }
                 else if (i == 1)
                 {
-                    Console.WriteLine("Proseguendo nel tuo cammino, vieni attaccato da una ChainComp, ma visto che sei un dinosauro ti basta fare solo un buon attacco per sconfiggerlo");
+                    if (p > 0)
+                    {
+                        Console.WriteLine("Proseguendo nel tuo cammino, vieni attaccato da una ChainComp, ma visto che sei un dinosauro ti basta fare solo un buon attacco per sconfiggerlo, ti basterà solamente totalizzare un puntaggio maggiore di 3 nel tiro di un dado per riuscire a sconfiggerlo");
+                        int TiroDado3 = rand.Next(1, 7);
 
+                        if (TiroDado3 > 3)
+                        {
+                            Console.WriteLine("Valore tiro dado: " + TiroDado3);
+                            Console.WriteLine("Sei riuscito a sconfiggere Chain Chomp! Come ricompensa per la tua vittoria, ottieni un pezzo di cavalcatura!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Valore tiro dado: " + TiroDado3);
+                            Console.WriteLine("Non sei riuscito a sconfiggere la ChainComp, ora lui ti ha colpito e sei tornato di nuovo nel corpo di " + personaggi + "! Prova a sconfiggerlo.");
 
+                            while (ChainChompVita > 0)
+                            {
+                                int TiroDado4 = rand.Next(1, 7);
+
+                                if (TiroDado4 <= 3)
+                                {
+                                    Console.WriteLine("Valore tiro dado: " + TiroDado4);
+                                    Console.WriteLine("Non sei riuscito ad attaccare Chain Chomp, ora lui ti ha attaccato e ti ha inflitto " + DinosauroAttacco + " danni! Premi un tasto per continuare...");
+                                    Console.ReadLine();
+                                    vita -= ChainChompAttacco;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Valore tiro dado: " + TiroDado4);
+                                    Console.WriteLine("Sei riuscito ad attaccare Chain Chomp, gli hai inflitto " + attacco + " danni! Premi un tato per continuare...");
+                                    Console.ReadLine();
+                                    ChainChompVita -= attacco;
+                                }
+                            }
+
+                            Console.WriteLine("Sei riuscito a sconfiggere Chain Chomp! Come ricompensa per la tua vittoria, ottieni un pezzo di cavalcatura!");
+                        }
+                        
+                    }
+                    else if(l > 0)
+                    {
+                        Console.WriteLine("Proseguendo nel tuo cammino, vieni attaccato da una ChainComp! Una palla di ferro legata a terra da una catena. Attaccala per continuare il viaggio.");
+
+                        while (ChainChompVita > 0)
+                        {
+                            int TiroDado = rand.Next(1, 7);
+
+                            if (TiroDado <= 3)
+                            {
+                                Console.WriteLine("Valore tiro dado: " + TiroDado);
+                                Console.WriteLine("Non sei riuscito ad attaccare il dinosauro, ora lui ti ha attaccato e ti ha inflitto " + ChainChompAttacco + " danni! Premi un tasto per continuare...");
+                                Console.ReadLine();
+                                vita -= ChainChompAttacco;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Valore tiro dado: " + TiroDado);
+                                Console.WriteLine("Sei riuscito ad attaccare il dinosauro, gli hai inflitto " + attacco + " danni! Premi un tato per continuare...");
+                                Console.ReadLine();
+                                ChainChompVita -= attacco;
+                            }
+                        }
+                    }
                 }
                 else if (i == 2)
                 {
                     Console.WriteLine("Dopo aver sconfitto la ChainComp, vieni fermato da MadameBrode!");
-                    //Qui andrebbe il combattimento contro MadameBrode
+                    Console.WriteLine("MadameBrode è la madre dei broodals, gli scagnozzi di Bowser che cercano di rapire Peach.Ti accorgi che lei ha le chiavi della Odissey e le tiene strette, sconfiggila per riuscire a prendere le chiavi e poter viaggiare tra i mondi.");
+
+                    while (MadameBroodeVita > 0)
+                    {
+                        int TiroDado5 = rand.Next(1, 7);
+
+                        if (TiroDado5 <= 3)
+                        {
+                            Console.WriteLine("Valore tiro dado: " + TiroDado5);
+                            Console.WriteLine("Non sei riuscito ad attaccare il MadameBroode, ora lei ti ha attaccato e ti ha inflitto " + MadameBroodeAttacco + " danni! Premi un tasto per continuare...");
+                            Console.ReadLine();
+                            vita -= MadameBroodeAttacco;
+
+                            if (MadameBroodeVita <= 10)
+                            {
+                                Console.WriteLine("MadameBroode si è infuriata, ora lei fa 4 di danno e ha usato una pozione di cura che le restituisce 1 di vita ad ogni turno");
+                                MadameBroodeAttacco = 4;
+                                MadameBroodeVita++;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Valore tiro dado: " + TiroDado5);
+                            Console.WriteLine("Sei riuscito ad attaccare MadameBroode, gli hai inflitto " + attacco + " danni! Premi un tato per continuare...");
+                            Console.ReadLine();
+                            MadameBroodeVita -= attacco;
+
+                            if (MadameBroodeVita <= 10)
+                            {
+                                Console.WriteLine("MadameBroode si è infuriata, ora lei fa 4 di danno e ha usato una pozione di cura che le restituisce 1 di vita ad ogni turno");
+                                MadameBroodeAttacco = 4;
+                                MadameBroodeVita++;
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -182,7 +284,7 @@ namespace ViaggioControBowser
                 Console.WriteLine("Hai scelto Luigi come protagonista!");
             }
 
-            Fossilandia("Mario", Vita, Attacco, Inventario);
+            Fossilandia(Personaggi(Vita, Attacco), Vita, Attacco, Inventario);
 
             for (int i = 0; i < Mappa.Length; i++)
             {
